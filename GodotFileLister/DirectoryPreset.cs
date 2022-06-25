bb0279ac176dedfea7283242baf9eb308436759b
Evgeny Karasev <yukikaze.interactive@gmail.com>
2022-06-25 19:14:41 +0300

    Implement directory preset data format

---
+++ GodotFileLister/DirectoryPreset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CsvHelper.Configuration.Attributes;
+
+namespace GodotFileLister
+{
+    public class DirectoryPreset
+    {
+        public DirectoryPreset(string path, string extension, string outputFileName)
+        {
+            Path = path;
+            Extension = extension;
+            OutputFileName = outputFileName;
+        }
+
+        [Index(0)]
+        public string Path { get; private set; }
+        [Index(1)]
+        public string Extension { get; private set; }
+        [Index(2)]
+        public string OutputFileName { get; private set; }
+    }
+}
--- GodotFileLister/Program.cs
+++ GodotFileLister/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace GodotFileLister
 {
@@ -6,7 +12,40 @@ namespace GodotFileLister
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string projectDirectory = "";
+            string presetFile = "presets.txt";
+
+            List<DirectoryPreset> presetList = LoadDirectoryPresetList($"{AppContext.BaseDirectory}\\{presetFile}");
+
+            foreach (DirectoryPreset preset in presetList)
+            {
+                Console.WriteLine($"{preset.Path}   {preset.Extension}  {preset.OutputFileName}");
+            }
+
+        }
+
+        static List<DirectoryPreset> LoadDirectoryPresetList(string presetFilePath)
+        {
+            List<DirectoryPreset> list = new List<DirectoryPreset>();
+
+            // Set up CsvHelper configuration
+            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+                Delimiter = "\t",
+            };
+
+            // Load the specified file as a list
+            if (File.Exists(presetFilePath))
+            {
+                using (StreamReader reader = new StreamReader(presetFilePath))
+                using (CsvReader csv = new CsvReader(reader, config))
+                {
+                    list = csv.GetRecords<DirectoryPreset>().ToList();
+                }
+            }
+
+            return list;
         }
     }
 }
