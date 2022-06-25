853e491eef3a82e616742810ec027e81e40be784
Evgeny Karasev <yukikaze.interactive@gmail.com>
2022-06-25 18:55:54 +0300

    Create a new C# console app project

---
+++ GodotFileLister/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GodotFileLister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
